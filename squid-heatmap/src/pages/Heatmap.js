c80560bff35d4d24c657f90e0993e71cc32df213
ARBT <aaron.torres@wvsu.edu.ph>
2025-04-09 01:29:09 +0800

    reclean

--- squid-heatmap/src/pages/Heatmap.js
+++ squid-heatmap/src/pages/Heatmap.js
@@ -200,7 +200,7 @@ const Heatmap = () => {
     setShowModal(false);
     setLoading(true);
     try {
-      const response = await fetch('https://indian-squid.onrender.com', {
+      const response = await fetch('http://localhost:5000/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
